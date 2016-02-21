caae7c9e2a3b86e27f6652adbc66e880227f4637
Daniel Cleveland <dpcleveland@gmail.com>
2016-02-20 21:38:59 -0600

    Add scripts, lightbox

---
+++ custom-scripts.js
@@ -0,0 +1,6 @@
+$(function() {
+	var elems = $('.picture');
+	elems.on('mouseenter mouseleave', function(e) {
+		elems.not(this).stop(true).fadeTo('fast', e.type == 'mouseenter' ? 0.5 : 1);
+	});
+});
