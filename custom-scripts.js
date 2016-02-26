c700d1de81481e22496a04b3b34ed8ec0bf5b314
Daniel Cleveland <dpcleveland@gmail.com>
2016-02-26 16:08:22 -0600

    Fixed external CSS/JS

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,8 +1,8 @@
 // Grey out other pictures on mouseover
 // Call with .picture class
-$(function() {
+$(function () {
 	var elems = $('.picture');
-	elems.on('mouseenter mouseleave', function(e) {
+	elems.on('mouseenter mouseleave', function (e) {
 		elems.not(this).stop(true).fadeTo('fast', e.type == 'mouseenter' ? 0.5 : 1);
 	});
 });
